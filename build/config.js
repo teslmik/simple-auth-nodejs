2c4dbcb90a110b10fd5d60462a7eec905d181fbf
teslmik <teslmik@gmail.com>
2023-05-26 22:53:33 +0300

    del module

--- build/config.js
+++
@@ -1,3 +0,0 @@
-import { createSecretKey } from "crypto";
-export const secret = createSecretKey("SECRET_KEY_RANDOM", "utf-8");
-//# sourceMappingURL=config.js.map
--- build/models/role.model.js
+++
@@ -1,6 +0,0 @@
-import { Schema, model } from 'mongoose';
-const Role = new Schema({
-    value: { type: String, unique: true, default: 'USER' },
-});
-export default model('Role', Role);
-//# sourceMappingURL=role.model.js.map
